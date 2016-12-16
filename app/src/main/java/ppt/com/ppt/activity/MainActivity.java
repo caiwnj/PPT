30883510b5969dad38ee24a7feaf3a1c11057819
Caiwnj <caiwnj@126.com>
2016-12-16 15:56:02 +0800

    201612161554

--- app/src/main/java/ppt/com/ppt/activity/MainActivity.java
+++ app/src/main/java/ppt/com/ppt/activity/MainActivity.java
@@ -5,23 +5,34 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
 import ppt.com.ppt.R;
 import ppt.com.ppt.fragment.BaseFragment;
+import ppt.com.ppt.fragment.FragmentFactory;
 import ppt.com.ppt.utils.MyUtil;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TabLayout tab_main;
-    private ViewPager vp_main;
     private String[] mTabs;
     private Toolbar toolBar;
+    private FrameLayout fl_content;
+    private FragmentManager supportFm;
+    private LinearLayout ll_collection;
+    private ActionBarDrawerToggle toggle;
+    private DrawerLayout drawer;
+    private LinearLayout ll_comment;
+    private BaseFragment baseFragment;
+    private String mCurrentFragment = MyUtil.getString(R.string.YOUMA);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,36 +43,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        initToolBar();
-        tab_main = (TabLayout) findViewById(R.id.tab_main);
-        vp_main = (ViewPager) findViewById(R.id.vp_main);
-        FragmentManager fm = getSupportFragmentManager();
-        HomeViewPagerAdapter homeAdapter = new HomeViewPagerAdapter(fm);
-        vp_main.setAdapter(homeAdapter);
-        tab_main.setupWithViewPager(vp_main);
+        ll_collection = (LinearLayout) findViewById(R.id.ll_collection);
+        ll_comment = (LinearLayout) findViewById(R.id.ll_comment);
+        fl_content = (FrameLayout) findViewById(R.id.fl_content);
+        initToolBarAndDrawble();
+        supportFm = getSupportFragmentManager();
+        supportFm.beginTransaction().add(R.id.fl_content, FragmentFactory.getFragment(mCurrentFragment), mCurrentFragment).commit();
+        ll_collection.setOnClickListener(this);
+        ll_comment.setOnClickListener(this);
     }
 
-    private void initToolBar() {
+    private void initToolBarAndDrawble() {//初始化ToolBar
         toolBar = (Toolbar) findViewById(R.id.toobal);
         setSupportActionBar(toolBar);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawer, toolBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
     }
 
-    class HomeViewPagerAdapter extends FragmentPagerAdapter {
-
-        public HomeViewPagerAdapter(FragmentManager fm) {
-            super(fm);
+    public void onClick(View v) {//点击侧边栏的条目，切换fragment
+        switch (v.getId()) {
+            case R.id.ll_collection:
+                baseFragment = new BaseFragment();
+                supportFm.beginTransaction().replace(R.id.fl_content, baseFragment, MyUtil.getString(R.string.YOUMA)).commit();
+                drawer.closeDrawers();
+                toolBar.setTitle("收藏");
+                break;
+            case R.id.ll_comment:
+                supportFm.beginTransaction().hide(baseFragment).commit();
+                drawer.closeDrawers();
+                toolBar.setTitle("评论");
+                break;
+            default:
+                break;
         }
 
-        public Fragment getItem(int position) {
-            return new BaseFragment();
-        }
+    }
 
-        public int getCount() {
-            return mTabs.length;
-        }
-
-        public CharSequence getPageTitle(int position) {
-            return mTabs[position];
+    public void ChangeContent(String fragmentName) {
+        if (mCurrentFragment == fragmentName) {
+            return;
+        } else {
+            Fragment fragment = supportFm.findFragmentByTag(mCurrentFragment);
+            Fragment fragment2Replace = supportFm.findFragmentByTag(fragmentName);
+            if (fragment2Replace == null) {
+                supportFm.beginTransaction().hide(fragment).add(FragmentFactory.getFragment(fragmentName), fragmentName).commit();
+            } else {
+                supportFm.beginTransaction().hide(fragment).show(fragment2Replace).commit();
+            }
+            mCurrentFragment = fragmentName;
         }
     }
 }
--- app/src/main/java/ppt/com/ppt/fragment/FragmentFactory.java
+++ app/src/main/java/ppt/com/ppt/fragment/FragmentFactory.java
@@ -1,11 +1,42 @@
 package ppt.com.ppt.fragment;
 
+import java.util.HashMap;
+
+import ppt.com.ppt.R;
+import ppt.com.ppt.utils.MyUtil;
+
 /**
  * Created by Caiwnj on 2016/12/15.
  */
 
 public class FragmentFactory {
-    public void getFragment(int pos) {
+    private static HashMap<String, BaseFragment> fragmentHashMap = new HashMap<String, BaseFragment>();
 
+    public static BaseFragment getFragment(String name) {
+        BaseFragment fragment;
+        fragment = fragmentHashMap.get(name);
+        if (fragment == null) {
+
+        }
+        return fragment;
+    }
+
+
+    public enum FragmentName {//用枚举类来代表所有的Fragment的类别
+
+        Youma(MyUtil.getString(R.string.YOUMA)),
+        Find(MyUtil.getString(R.string.FIND)),
+        Message(MyUtil.getString(R.string.MESSAGE)),
+        Me(MyUtil.getString(R.string.ME));
+        private String name;
+
+        private FragmentName(String name) {
+            this.name = name;
+        }
+
+        public String getname() {
+
+            return name;
+        }
     }
 }
